84ab6db0c4550a6ea5486fd0c290d1fec30c9655
Yinon Tzomi <yinontz@edu.hac.ac.il>
2022-04-13 18:50:25 +0300

    Added decipher logic

--- picture.py
+++ picture.py
@@ -1,5 +1,22 @@
 from PIL import Image
 
+
+def decipher():
+    """
+    https://stackoverflow.com/questions/56441769/iterate-over-all-pixels-to-check-which-pixels-are-white-and-which-are-black
+    Open file and decipher code.
+    :return: String decrypted from image.
+    """
+    black = 1
+
+    with Image.open("code.png").convert() as img:
+        pixel = img.load()
+
+        return "".join([chr(height)
+                        for width in range(img.size[0])
+                        for height in range(img.size[1])
+                        if pixel[width, height] == black])
+
+
 if __name__ == '__main__':
-    with Image.open("code.png") as im:
-        im.show()
+    print(decipher())
