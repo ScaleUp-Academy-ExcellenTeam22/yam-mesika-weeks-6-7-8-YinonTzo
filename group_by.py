d05fccf2fae924adae8d9a7fb9a6a82ef70c2c0c
Yinon Tzomi <yinontz@edu.hac.ac.il>
2022-05-09 20:45:21 +0300

    Added description to group_by and change name to return value

--- group_by.py
+++ group_by.py
@@ -3,15 +3,18 @@ from typing import Callable, Iterable
 
 def group_by(function: Callable, iterable: Iterable) -> dict:
     """
+    Go over all values in iterable and run the function on them.
+    Return a dictionary with return values from function as a key
+    and value corresponding to a particular key from the iterable.
     :param function: Function run on all values in iterable.
     :param iterable: Arguments for function.
     :return: A dictionary with function(value) as a key and all values (with this key)
     as a list.
     """
-    ret = {}
+    return_dict = {}
     for value in iterable:
-        ret.setdefault(function(value), []).append(value)
-    return ret
+        return_dict.setdefault(function(value), []).append(value)
+    return return_dict
 
 
 if __name__ == '__main__':
